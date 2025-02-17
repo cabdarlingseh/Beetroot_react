94c8311aaaa60720408a07f4a230f017167e5536
cabdarlingseh <david_cj7@protonmail.com>
2025-02-17 18:24:55 +0000

    movie DB

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import SearchInput from "./SearchInput";
+
+
+export default function Home() {
+    return (
+        <div className="container">
+            <SearchInput />
+        </div>
+    )
+}
